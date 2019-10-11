2afda5ae953502a4992e756857eb2a7b92fe8ba8
Suzanne <14323418+kernowork@users.noreply.github.com>
2019-10-10 18:15:45 -0700

    Delete contentassets.d.ts

--- .sfdx/typings/lwc/contentassets.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@salesforce/contentAssetUrl/AttendanceIcon" {
-    var AttendanceIcon: string;
-    export default AttendanceIcon;
-}
-declare module "@salesforce/contentAssetUrl/pawns" {
-    var pawns: string;
-    export default pawns;
-}
