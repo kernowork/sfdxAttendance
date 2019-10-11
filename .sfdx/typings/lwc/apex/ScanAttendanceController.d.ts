5ed61c6afb9560a83fdb2fba0f58a5545f0cb987
Suzanne <14323418+kernowork@users.noreply.github.com>
2019-10-10 18:17:16 -0700

    Delete ScanAttendanceController.d.ts

--- .sfdx/typings/lwc/apex/ScanAttendanceController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/ScanAttendanceController.getContacts" {
-  export default function getContacts(param: {barcode: any}): Promise<any>;
-}
-declare module "@salesforce/apex/ScanAttendanceController.saveAttendance" {
-  export default function saveAttendance(param: {con: any, recordId: any}): Promise<any>;
-}
