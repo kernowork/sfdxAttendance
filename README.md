f6e456f7232d3d04adf9954fa7f9200a318f2228
Kernowork <14323418+kernowork@users.noreply.github.com>
2020-01-01 14:33:31 -0800

    Add Aura components to create sessions

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Assign "Attendance" permission set to access the app.
 
 The LoadSampleData component on the Attendance Home Page loads sample data for a test drive.
 Contacts are loaded from the Static Resource MASHPeople.csv.  Enjoy!
+
+Program Utilities page has components to facilitate setting up program sessions for today, or for upcoming weeks.
--- force-app/main/default/aura/CreateProgramSessionsToday/CreateProgramSessionsTodayController.js
+++ force-app/main/default/aura/CreateProgramSessionsToday/CreateProgramSessionsTodayController.js
@@ -28,33 +28,29 @@
 
 
     addSelected: function (component, event, helper) {
-        console.log('addSelected');
         //create array for temporary storage of Ids of checked participants
-
-        var boxes = ["checkBox0", "checkBox1", "checkBox2", "checkBox3", "checkBox4"];
+        var tempIDs = [];
 
         //get all checkboxes
-        // for (var n = 0; n < 1; n++) {
-            var tempIDs = [];
-            var getAllId = component.find('checkBox0');
-            
-            //save the ones that are checked
-            if (getAllId != null) {
-                if (!Array.isArray(getAllId) && (getAllId.get("v.checked") == true && getAllId.get("v.disabled") == false)) {
-                    tempIDs.push(getAllId.get("v.value"));  //in the unlikely case there is only one entry in the form!
-                } else {
-                    for (var i = 0; i < getAllId.length; i++) {
-                        if (getAllId[i].get("v.checked") == true && getAllId[i].get("v.disabled") == false) {
-                            tempIDs.push(getAllId[i].get("v.value"));
-                        }
+        var getAllId = component.find('checkBox0');
+
+        //save the ones that are checked
+        if (getAllId != null) {
+            if (!Array.isArray(getAllId) && (getAllId.get("v.checked") == true && getAllId.get("v.disabled") == false)) {
+                tempIDs.push(getAllId.get("v.value"));  //in the unlikely case there is only one entry in the form!
+            } else {
+                for (var i = 0; i < getAllId.length; i++) {
+                    if (getAllId[i].get("v.checked") == true && getAllId[i].get("v.disabled") == false) {
+                        tempIDs.push(getAllId[i].get("v.value"));
                     }
                 }
-
-                console.log(tempIDs);
-                helper.saveSessions(component, event, tempIDs, 1);
             }
-            console.log('getAllId is null');
-        // }
+
+            console.log(tempIDs);
+            helper.saveSessions(component, event, tempIDs, 1);
+        }
+        console.log('getAllId is null');
+
     },
 
 
